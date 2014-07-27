29a29afbdfa318069f74c828a38edf6fd51df7c3
ChaseDo <Chase.Do@qq.com>
2014-07-27 18:38:22 +0800

    Client source custom
    Client source custom

---
+++ KN_Order_Storage/Scripts/client.js
@@ -0,0 +1,13 @@
+﻿//$("#sourceDropDown").select(
+//    function () {
+//        var source = $("#sourceDropDown").val();
+//        if (source == "其他") {
+//            $("#sourceCSTEditor").enabled;
+//            return;
+//        }
+//        else {
+//            $("#sourceCSTEditor").disabled;
+//        }
+//    }
+//);
+$("#weddingdayEditor").datepicker();
